7be0d3a93889b8090c8d655d8fd2b011153a82c2
etneMr <91597719+etneMr@users.noreply.github.com>
2023-07-12 15:38:51 +0700

    ECO-Header use flex

--- src/presentation/components/common/Header/Header.jsx
+++ src/presentation/components/common/Header/Header.jsx
@@ -16,27 +16,28 @@ export function Header() {
                 <img src={ImageLogo} alt="logo" className="image" />
                 <div className="name">Furniro</div>
             </Link>
-            <ul id="header-right">
-                <li key="home">
-                    <Link className="item" to="/">Home</Link>
-                </li>
-                <li key="shop">
-                    <Link className="item" to="/shop">Shop</Link>
-                </li>
-                <li key="about">
-                    <Link className="item" to="/about">About</Link>
-                </li>
-                <li key="contact">
-                    <Link className="item" to="/contact">Contact</Link>
 
-                </li>
-            </ul>
+            <div id="header-right">
+                <div key="home">
+                    <Link className="item" to="/">Home</Link>
+                </div>
+                <div key="shop">
+                    <Link className="item" to="/shop">Shop</Link>
+                </div>
+                <div key="about">
+                    <Link className="item" to="/about">About</Link>
+                </div>
+                <div key="contact">
+                    <Link className="item" to="/contact" style={{ paddingRight: 0 }}>Contact</Link>
+
+                </div>
+            </div>
 
             <div id="header-icon">
                 <img className="icon" src={IconProfile} alt="Profile" />
                 <img className="icon" src={IconSearch} alt="Search" />
                 <img className="icon" src={IconFavorite} alt="Favorite" />
-                <img className="icon" src={IconCart} alt="Cart" />
+                <img className="icon" src={IconCart} alt="Cart" style={{ paddingRight: 0 }} />
             </div>
         </div>
     );
