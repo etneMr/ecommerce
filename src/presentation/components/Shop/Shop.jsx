bfe39eec2a59af5eeb37d27b3513f236a4d356b9
etneMr <91597719+etneMr@users.noreply.github.com>
2023-07-13 11:01:27 +0700

    ECO-Shop add shop filter ui

--- src/presentation/components/Shop/Shop.jsx
+++ src/presentation/components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer, Header } from "../common";
 import ShopBanner from "../ShopBanner/ShopBanner";
+import ShopFilter from "../ShopFilter/ShopFilter";
 
 class Shop extends React.Component {
     render() {
@@ -8,6 +9,7 @@ class Shop extends React.Component {
             <>
                 <Header />
                 <ShopBanner />
+                <ShopFilter />
                 <h1>Welcome Shop</h1>
                 <Footer />
             </>
